6973c381578ab74fb9b66a24cdebb8f3c2e10731
FatmaDaify <fdaify@iskydev.com>
2018-10-01 00:17:59 +0200

    [UPD] comment name_search method in partner

--- hebeish_custom_report/models/res_partner.py
+++ hebeish_custom_report/models/res_partner.py
@@ -14,13 +14,13 @@ class ResPartner(models.Model):
         ('partnercode_unique', 'unique(partner_code)', 'Code already exists!')
     ]
 
-    @api.model
-    def name_search(self, name, args=None, operator='ilike', limit=100):
-        args = args or []
-        domain = []
-        if name:
-            domain = ['|', ('partner_code', 'ilike', name + '%'), ('name', operator, name)]
-            if operator in expression.NEGATIVE_TERM_OPERATORS:
-                domain = ['&', '!'] + domain[1:]
-        accounts = self.search(domain + args, limit=limit)
-        return accounts.name_get()
+    # @api.model
+    # def name_search(self, name, args=None, operator='ilike', limit=100):
+    #     args = args or []
+    #     domain = []
+    #     if name:
+    #         domain = ['|', ('partner_code', 'ilike', name + '%'), ('name', operator, name)]
+    #         if operator in expression.NEGATIVE_TERM_OPERATORS:
+    #             domain = ['&', '!'] + domain[1:]
+    #     accounts = self.search(domain + args, limit=limit)
+    #     return accounts.name_get()
