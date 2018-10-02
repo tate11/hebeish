52875ea4c57b111c5431ba069b6f69af5c5ccfa6
FatmaDaify <fdaify@iskydev.com>
2018-10-02 23:14:41 +0200

    [UPD] product barcode

--- bi_product_barcode_customization/models/product_template_inherit.py
+++ bi_product_barcode_customization/models/product_template_inherit.py
@@ -1,4 +1,3 @@
-
 from odoo import api, fields, models, _
 from random import randint
 
@@ -15,17 +14,15 @@ class product_template_inherit(models.Model):
         while barcode_exist:
             random_barcode = (randint(1000000000000, 9999999999999))
             barcode_exist = self.env['product.template'].search([('barcode', '=', random_barcode)])
-
         vals['barcode'] = str(random_barcode)
         return super(product_template_inherit, self).create(vals)
 
     @api.model
     def product_generate_barcode(self):
-        for record in self.env['product.template'].search([('barcode','=',False)]):
+        for record in self.env['product.template'].search([('barcode', '=', False)]):
             random_barcode = (randint(1000000000000, 9999999999999))
             barcode_exist = self.env['product.template'].search([('barcode', '=', random_barcode)])
             while barcode_exist:
                 random_barcode = (randint(1000000000000, 9999999999999))
                 barcode_exist = self.env['product.template'].search([('barcode', '=', random_barcode)])
-
             record.update({'barcode': str(random_barcode)})
