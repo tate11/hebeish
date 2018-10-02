4eefcc09e4591a0c00a1b21ff2f72c0bdfd1a41d
FatmaDaify <fdaify@iskydev.com>
2018-10-02 22:36:52 +0200

    [UPD] add partner view

--- hebeish_custom_report/__manifest__.py
+++ hebeish_custom_report/__manifest__.py
@@ -3,18 +3,17 @@
     'version': '10.0',
     'category': 'Quality',
     'author': 'Maha Hamza <maha.hamza@acme-group.net>',
-    'depends': ['base','quality','mrp'],
+    'depends': ['base', 'quality', 'mrp'],
     'data': [
         'security/quality_data.xml',
         'security/ir.model.access.csv',
         'data/decimal_precision.xml',
         'reports/final_report_template.xml',
         'reports/project_qweb_report.xml',
-        'views/quality_report_views.xml'
+        'views/quality_report_views.xml',
+        'views/res_partner_inherit_view.xml',
     ],
     'installable': True,
     'application': False,
     'auto_install': False,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
